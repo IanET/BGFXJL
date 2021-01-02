260a5e9d3415cfaef1ec5ccde654f0fca747f3ea
Ian Ellison-Taylor <ian@ellison-taylor.com>
2021-01-02 13:42:07 -0800

    Julia is row major

--- TestBgfx.jl
+++ TestBgfx.jl
@@ -16,6 +16,8 @@ include("Bgfx.jl")
 const Vector3 = Tuple{Float32, Float32, Float32}
 const Vector4 = Tuple{Float32, Float32, Float32, Float32}
 const Matrix4x4 = Matrix{Float32}
+
+# NB Julia is column major and c expects row order
 Matrix4x4() = Matrix{Float32}(undef, 4, 4)
 Matrix4x4(x::Float32) = fill(x, (4, 4))
 Matrix4x4(I::UniformScaling{Bool}) = Matrix{Float32}(I, 4, 4)
@@ -31,32 +33,35 @@ Base.convert(::Type{PosColorVertex}, x::Array) = PosColorVertex(Float32(x[1]), F
 function createLookAt(camera::Vector3, target::Vector3, up::Vector3)::Matrix4x4
     camera = collect(camera)
     target = collect(target)
+    up = collect(up)
     zaxis = normalize(camera - target)
-    xaxis = normalize(cross(collect(up), zaxis))
+    xaxis = normalize(cross(up, zaxis))
     yaxis = cross(zaxis, xaxis)
 
     lookat = Matrix4x4()
     lookat[1,1] = xaxis[1]
-    lookat[1,2] = yaxis[1]
-    lookat[1,3] = zaxis[1]
-    lookat[1,4] = 0
-    
-    lookat[2,1] = xaxis[2]
+    lookat[2,1] = yaxis[1]
+    lookat[3,1] = zaxis[1]    
+    lookat[4,1] = 0.0f0
+
+    lookat[1,2] = xaxis[2]
     lookat[2,2] = yaxis[2]
-    lookat[2,3] = zaxis[2]
-    lookat[2,4] = 0
+    lookat[3,2] = zaxis[2]
+    lookat[4,2] = 0.0f0
 
-    lookat[3,1] = xaxis[3]
-    lookat[3,2] = yaxis[3]
+    lookat[1,3] = xaxis[3]
+    lookat[2,3] = yaxis[3]
     lookat[3,3] = zaxis[3]
-    lookat[3,4] = 0
+    lookat[4,3] = 0.0f0
 
-    lookat[4,1] = -dot(xaxis, camera)
-    lookat[4,1] = -dot(yaxis, camera)
-    lookat[4,1] = -dot(zaxis, camera)
-    lookat[4,4] = 1
+    lookat[1,4] = -dot(xaxis, camera)
+    lookat[2,4] = -dot(yaxis, camera)
+    lookat[3,4] = -dot(zaxis, camera)
+    lookat[4,4] = 1.0
 
-    return lookat
+    # @show lookat
+    
+return lookat
 end
 
 function createPerspectiveFieldOfView(fieldOfView::Float32, aspectRatio::Float32, nearPlaneDistance::Float32, 
@@ -68,8 +73,8 @@ function createPerspectiveFieldOfView(fieldOfView::Float32, aspectRatio::Float32
     fov[1,1] = xScale
     fov[2,2] = yScale;
     fov[3,3] = farPlaneDistance / (nearPlaneDistance - farPlaneDistance)
-    fov[3,4] = -1.0
-    fov[4,3] = nearPlaneDistance * farPlaneDistance / (nearPlaneDistance - farPlaneDistance)
+    fov[3,4] = (nearPlaneDistance * farPlaneDistance) / (nearPlaneDistance - farPlaneDistance)
+    fov[4,3] = -1.0
 
     return fov;
 end
@@ -103,23 +108,33 @@ function createFromYawPitchRoll(yaw::Float32, pitch::Float32, roll::Float32)::Ma
     yz = y * z
     wx = x * w
 
-    result = Matrix4x4(I)
+    result = Matrix4x4()
     result[1,1] = 1.0 - 2.0 * (yy + zz)
-    result[1,2] = 2.0 * (xy + wz)
-    result[1,3] = 2.0 * (xz - wy)
-    result[2,1] = 2.0 * (xy - wz)
+    result[2,1] = 2.0 * (xy + wz)
+    result[3,1] = 2.0 * (xz - wy)
+    result[4,1] = 0.0
+
+    result[1,2] = 2.0 * (xy - wz)
     result[2,2] = 1.0 - 2.0 * (zz + xx)
-    result[2,3] = 2.0 * (yz + wx)
-    result[3,1] = 2.0 * (xz + wy)
-    result[3,2] = 2.0 * (yz - wx)
+    result[3,2] = 2.0 * (yz + wx)
+    result[4,2] = 0.0
+
+    result[1,3] = 2.0 * (xz + wy)
+    result[2,3] = 2.0 * (yz - wx)
     result[3,3] = 1.0 - 2.0 * (yy + xx)
+    result[4,3] = 0.0
+
+    result[1,4] = 0.0
+    result[2,4] = 0.0
+    result[3,4] = 0.0
+    result[4,4] = 1.0
 
     return result
 end
 
 function main()
-    width = 800
-    height = 600
+    width = 1280
+    height = 720
     viewID = UInt16(0)
 
     GLFW.Init()
@@ -144,7 +159,7 @@ function main()
         [-1.0, -1.0, -1.0, 0xffffff00],
         [ 1.0, -1.0, -1.0, 0xffffffff]
     ]
-    
+
     cubeTriList = UInt16[
         0, 1, 2,
         1, 3, 2,
@@ -171,8 +186,8 @@ function main()
 
     layout = Ref(bgfx_vertex_layout_t(0, 0, tuple(zeros(UInt16, 18)...), tuple(zeros(UInt16, 18)...)))
     bgfx_vertex_layout_begin(layout, BGFX_RENDERER_TYPE_NOOP)
-    bgfx_vertex_layout_add(layout, BGFX_ATTRIB_POSITION, UInt8(3), BGFX_ATTRIB_TYPE_FLOAT, false, false)
-    bgfx_vertex_layout_add(layout, BGFX_ATTRIB_COLOR0, UInt8(4), BGFX_ATTRIB_TYPE_UINT8, true, false)
+    bgfx_vertex_layout_add(layout, BGFX_ATTRIB_POSITION, 0x03, BGFX_ATTRIB_TYPE_FLOAT, false, false)
+    bgfx_vertex_layout_add(layout, BGFX_ATTRIB_COLOR0, 0x04, BGFX_ATTRIB_TYPE_UINT8, true, false)
     bgfx_vertex_layout_end(layout)
     memVerts = bgfx_copy(cubeVertices, sizeof(cubeVertices) |> UInt32)
     vbh = bgfx_create_vertex_buffer(memVerts, layout, BGFX_BUFFER_NONE)
@@ -185,12 +200,14 @@ function main()
     startTime = time()
     while !GLFW.WindowShouldClose(window)
         GLFW.PollEvents()
-        bgfx_set_view_rect(viewID, UInt16(0), UInt16(0), UInt16(width), UInt16(height))
-        bgfx_touch(viewID)
+        bgfx_set_view_rect(viewID, 0x0000, 0x0000, UInt16(width), UInt16(height))
 
         viewMatrix = createLookAt((0.0f0, 0.0f0, -35.0f0), (0.0f0, 0.0f0, 0.0f0), (0.0f0, 1.0f0, 0.0f0))
         projMatrix = createPerspectiveFieldOfView(Float32(π / 3.0), Float32(width / height), 0.1f0, 100.0f0)
+        projMatrix = createPerspectiveFieldOfView(Float32(π / 3), Float32(width / height), 0.10f0, 100.0f0)
         bgfx_set_view_transform(viewID, viewMatrix, projMatrix)
+
+        bgfx_touch(viewID)
 
         now = time()
         framedt = now - lastFrameTime
@@ -200,18 +217,18 @@ function main()
         bgfx_dbg_text_clear(0x00, false);
         bgfx_dbg_text_printf(0x0000, 0x0001, 0x1f, "Julia Cubes: $(UInt32(round(framedt*1000)))");
 
-        for y = 0:11 
+        for y = 0:11
             for x = 0:11
                 transform = createFromYawPitchRoll(Float32(startdt + x * 0.21), Float32(startdt + y * 0.37), 0.0f0)
-                transform[4,1] = -15.0 + x * 3.0
-                transform[4,2] = -15.0 + y * 3.0
-                transform[4,3] = 0.0
+                transform[1,4] = -15.0 + x * 3.0
+                transform[2,4] = -15.0 + y * 3.0
+                transform[3,4] = 0.0
                 bgfx_set_transform(transform, 0x0001)
 
                 bgfx_set_vertex_buffer(0x00, vbh, UInt32(0), length(cubeVertices) |> UInt32)
                 bgfx_set_index_buffer(ibh, UInt32(0), length(cubeTriList) |> UInt32)
-                bgfx_set_state(BGFX_STATE_DEFAULT, UInt32(0))
 
+                bgfx_set_state(BGFX_STATE_DEFAULT, UInt32(0))
                 bgfx_submit(viewID, program, UInt32(0), BGFX_DISCARD_ALL);
             end
         end
